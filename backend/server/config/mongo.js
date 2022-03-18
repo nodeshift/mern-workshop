766b18228aab2554be0c8b73042cc34c565345d4
Costas Papastathis <papastathiscr@gmail.com>
2022-03-18 17:04:11 +0200

    refactor: adding mongo port as environment variable

--- backend/server/config/mongo.js
+++ backend/server/config/mongo.js
@@ -1,5 +1,6 @@
 module.exports = {
   "mongoURL" : process.env.MONGO_URL || 'localhost',
+  "mongoPORT": process.env.MONOGO_PORT || '27017',
   "mongoUser" : process.env.MONGO_USER || '',
   "mongoPass" : process.env.MONGO_PASS || '',
   "mongoDBName" : process.env.MONGO_DB_NAME || 'todos',
