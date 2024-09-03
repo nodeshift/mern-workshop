6ab457709625db8e5b6a79b97d266cc1d38084fc
Costas Papastathis <papastathiscr@gmail.com>
2024-09-03 15:18:23 +0300

    removing deprecated variables

--- backend/server/config/mongo.js
+++ backend/server/config/mongo.js
@@ -4,12 +4,10 @@ module.exports = {
   mongoUser: process.env.MONGO_USER || "",
   mongoPass: process.env.MONGO_PASS || "",
   mongoDBName: process.env.MONGO_DB_NAME || "todos",
+  disableReplicaSet: process.env.MONGO_DISABLE_REPLICA_SET || false,
   connectOptions: {
-    useNewUrlParser: true,
     ssl: false,
-    sslValidate: false,
     socketTimeoutMS: 5000,
     replicaSet: process.env.MONGO_REPLICA_SET_NAME || "rs0",
-    useUnifiedTopology: true,
   },
 };
