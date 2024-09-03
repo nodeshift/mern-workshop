085b0dbf5df77cada72cc0712b8b169a64ed598e
Costas Papastathis <papastathiscr@gmail.com>
2024-09-03 15:19:49 +0300

    converting crud operation to async await from callback

--- backend/server/routers/routes.js
+++ backend/server/routers/routes.js
@@ -1,104 +1,109 @@
-'use strict';
+"use strict";
 
-const bodyParser = require('body-parser');
-const express = require('express');
-const Todo = require('../model/todos');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const express = require("express");
+const Todo = require("../model/todos");
+const cors = require("cors");
 
-module.exports = function(app) {
-
+module.exports = function (app) {
   let router = express.Router();
 
-  app.use(cors())
+  app.use(cors());
 
-  // set up other middleware
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
   app.use(bodyParser.json());
 
-  router.get('/todos', (req, res) => {
-    Todo.find(function(err, todos) {
-      if (err) {
-        res.status(503).send(err);
-      }
+  router.get("/todos", async (_, res) => {
+    try {
+      console.log("Attempting to fetch all todos");
+      const todos = await Todo.find();
       res.json(todos);
-    });
+      return;
+    } catch (err) {
+      console.log(err);
+      res.status(503).send;
+      return;
+    }
   });
 
-  router.post('/todos', (req, res) => {
+  router.post("/todos", async (req, res) => {
     const author = req.body.author;
     const task = req.body.task;
-		console.log(`Creating Todo for ${req.body.author} ${task}`);
+
+    console.log(`Creating Todo for ${req.body.author} ${task}`);
+
     if (!task || !author) {
       res.status(422).send("Unprocessable Entity");
       return;
     }
     const todo = new Todo({
       author: author,
-      task: task
+      task: task,
     });
 
-    todo.save((err) => {
+    try {
+      await todo.save();
+      res.json({
+        message: "Todo successfully added!",
+      });
+      return;
+    } catch (err) {
       console.log(err);
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.json({
-          message: 'Todo successfully added!'
-        });
-      }
-    });
+      return res.status(500).send(err);
+    }
   });
 
-  router.put('/todos/:todo_id', function(req, res) {
-    const todoID = req.params.todo_id
-    console.log(`Attempting to update a todo by ID (${todoID})`)
-    const task = req.body.task;
+  router.put("/todos/:todo_id", async function (req, res) {
+    const todoID = req.params.todo_id;
+    console.log(`Updating Todo by ID (${todoID})`);
 
-    Todo.findByIdAndUpdate(
-      req.params.todo_id,
-      req.body, {
+    try {
+      await Todo.findByIdAndUpdate(req.params.todo_id, req.body, {
         safe: true,
         upsert: true,
-        new: true
-      },
-      function(err, raw) {
-        if (err) {
-          res.status(503).send(err)
-        } else {
-          res.json({
-            message: `Updated todo ${todoID}`
-          })
-        }
-      })
+        new: true,
+      });
+      res.json({
+        message: `Updated todo ${todoID}`,
+      });
+      return;
+    } catch (err) {
+      res.status(503).send(err);
+      return;
+    }
   });
 
-  router.delete('/todos/:todo_id', (req, res) => {
-    Todo.deleteOne({
-      _id: req.params.todo_id
-    }, function(err, todo) {
-      if (err) {
-        res.status(503).send(err);
-      }
-      else{
-        res.json({
-          message: 'Todo has been deleted'
-        })
-      }
-    });
+  router.delete("/todos/:todo_id", async (req, res) => {
+    try {
+      await Todo.deleteOne({
+        _id: req.params.todo_id,
+      });
+      res.json({
+        message: "Todo has been deleted",
+      });
+      return;
+    } catch (err) {
+      res.status(503).send(err);
+      return;
+    }
   });
 
-  router.delete('/todos', (req, res) => {
-    console.log("Attempting to delete all todos")
-    Todo.deleteMany({}, function(err, todo) {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.send("Deleted todos data")
-      }
-    })
+  router.delete("/todos", async (req, res) => {
+    console.log("Attempting to delete all todos");
+
+    try {
+      await Todo.deleteMany({});
+      res.send("Deleted todos data");
+      return;
+    } catch (err) {
+      res.status(500).send(err);
+      return;
+    }
   });
 
-  app.use('/api', router);
+  app.use("/api", router);
 };
